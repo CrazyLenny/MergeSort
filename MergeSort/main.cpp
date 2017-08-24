5c092e39e5da06d536b079b3a6bc49aacce4dfcb
CrazyLenny <ykxlenny@gmail.com>
2017-08-24 17:08:51 -0400

    first commit

---
+++ MergeSort/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 24, 2017
+ *      Author: Kexin
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "first line of MergeSort!" << endl;
+	return 0;
+}
+
+
+
